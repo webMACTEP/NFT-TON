39d3c7c99531eb483540141cea7b782ea7203044
LGBesports <david17roma@gmail.com>
2022-05-18 20:42:35 +0300

    ависмичавпчса

--- js/script.js
+++ js/script.js
@@ -12,6 +12,12 @@ jQuery(document).ready(function ($) {
    let fog8 = document.querySelector('.fly8');
    let fog9 = document.querySelector('.fly9');
    let fog10 = document.querySelector('.fly10');
+   let light1 = document.querySelector('.light1');
+   let light2 = document.querySelector('.light2');
+   let light3 = document.querySelector('.light3');
+   let light4 = document.querySelector('.light4');
+   let metalball1 = document.querySelector('.metal-ball1');
+   let metalball2 = document.querySelector('.metal-ball2');
    window.addEventListener('mousemove', function (e) {
       let x = e.clientX / window.innerWidth;
       let y = e.clientY / window.innerHeight;
@@ -25,6 +31,12 @@ jQuery(document).ready(function ($) {
       fog8.style.transform = 'translate(-' + x * 70 + 'px, -' + y * 70 + 'px)';
       fog9.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
       fog10.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+      light1.style.transform = 'translate(-' + x * 40 + 'px, -' + y * 20 + 'px)';
+      light2.style.transform = 'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)';
+      light3.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 40 + 'px)';
+      light4.style.transform = 'translate(-' + x * 40 + 'px, -' + y * 20 + 'px)';
+      metalball1.style.transform = 'translate(-' + x * 60 + 'px, -' + y * 40 + 'px)';
+      metalball2.style.transform = 'translate(-' + x * 40 + 'px, -' + y * 30 + 'px)';
    });
 
 
