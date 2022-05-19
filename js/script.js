989dcf1c421bfc14abc08bf8cc66c7de2a73959f
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2022-05-19 14:59:58 +0300

    888

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ jQuery(document).ready(function ($) {
       const treeSequenceImages = [];
 
       for (let i = 0; i <= 131; i++) {
-            appleSequenceImages.push(`${`000${i}`.slice(-4)}.jpg`);
+            appleSequenceImages.push(`${`00${i}`.slice(-4)}.png`);
       }
       for (let i = 0; i <= 98; i++) {
             treeSequenceImages.push(`${`000${i}`.slice(-3)}.jpg`);
@@ -397,7 +397,7 @@ jQuery(document).ready(function ($) {
             container: '.apple-sequence',
             scrollWith: '.apple-container',
             images: appleSequenceImages,
-            imagesRoot: 'https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/02-head-bob-turn/',
+            imagesRoot: 'https://webmactep.github.io/NFT-TON/img/sequence/ufo/',
             priorityFrames: [0, 20, 40, 60, 90],
             cover: true,
             playUntil: 'scroll-out',
