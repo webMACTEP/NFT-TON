8bcb75b179fe3c05ac2bdf3f4342cb3529d898a5
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2022-05-23 09:33:43 +0300

    nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 jQuery(document).ready(function ($) {
 
+      const section = $('.section'),
+            nav = $('.menu'),
+            navHeight = nav.outerHeight(); // получаем высоту навигации 
+
+      // поворот экрана 
+      window.addEventListener('orientationchange', function () {
+            navHeight = nav.outerHeight();
+      }, false);
+
+      $(window).on('scroll', function () {
+            const position = $(this).scrollTop();
+
+            section.each(function () {
+                  const top = $(this).offset().top - navHeight - 5,
+                        bottom = top + $(this).outerHeight();
+
+                  if (position >= top && position <= bottom) {
+                        nav.find('a').removeClass('active');
+                        section.removeClass('active');
+
+                        $(this).addClass('active');
+                        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+                  }
+            });
+      });
+
+      nav.find('a').on('click', function () {
+            const id = $(this).attr('href');
+
+            $('html, body').animate({
+                  scrollTop: $(id).offset().top - navHeight
+            }, 487);
+
+            return false;
+      });
+
 
       var preloader = $('#preloader'), // селектор прелоадера
             imagesCount = $('img').length, // количество изображений
@@ -197,7 +233,7 @@ jQuery(document).ready(function ($) {
                         .width(winH / ratio)
                         .height(winH)
                         .css({
-                              
+
                               // marginLeft: - winH / ratio / 2 + "px",
                               left: "50%",
                               top: "0",
